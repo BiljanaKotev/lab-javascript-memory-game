bf2185c729148adf6771532d35e073d4a46c53d5
Biljana Kotevska <biljanakotev@gmail.com>
2023-06-04 19:35:14 +0200

    finished logic iterations except for shuffle

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,32 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
-  shuffleCards() {
-    // ... write your code here
-  }
+  shuffleCards() {}
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    if (card1 && card2) {
+      this.pairsClicked += 1;
+    }
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+    }
+    if (card1 === card2) {
+      return true;
+    } else if (card1 != card2) {
+      return false;
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true;
+    } else if (this.pairsGuessed === 0 || this.pairsGuessed != 0) {
+      return false;
+    }
   }
 }
